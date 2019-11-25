6dc66a724af747404d36227ed9463e66af2c7255
paulo giusti <paulo.giusti@lemon.energy>
2019-11-25 18:19:22 -0300

    chore: fix event and action validator

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const { exec } = require('@actions/exec')
 const { GITHUB_TOKEN } = process.env
 const VERSIONS = ['patch', ]
 const EVENT = 'pull_request'
-const EVENT_TYPE = 'labeled'
+const EVENT_ACTION = 'labeled'
 
 const checkEvent = () => {
   const { eventName, payload } = github.context
 
   core.debug(eventName, payload.action)
-  
+
   if (
     eventName === EVENT
-    && !payload.action
-    && payload.action !== EVENT_TYPE
+    && payload.action
+    && payload.action === EVENT_ACTION
   ) return
 
   throw Error('Event not supported')
