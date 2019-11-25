7892fa8e8decc731e4c00ea41abffb268f152f98
paulo giusti <paulo.giusti@lemon.energy>
2019-11-25 17:38:04 -0300

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const { exec } = require('@actions/exec')
+
+core.debug(github.context)
+// const { GITHUB_TOKEN } = process.env
+// const VERSIONS = ['patch', ]
+// const EVENT = 'pull_request'
+// const EVENT_TYPE = 'labeled'
+//
+// const checkActionEvent = () => {
+//   const { pull_request } = github.context.payload || {}
+//
+//   if (
+//     pull_request
+//     && !pull_request.action
+//     && pull_request.action !== 'labeled'
+//   ) return
+//
+//   throw Error('Event not supported')
+// }
+//
+//
+// const run = async () => {
+//
+// }
