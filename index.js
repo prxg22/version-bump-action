deff29cdcf7fe5f67cfb3192f28cf7f8bd882a01
paulo giusti <paulo.giusti@lemon.energy>
2019-11-25 17:48:03 -0300

    chore: echo github context

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const { exec } = require('@actions/exec')
 
-core.debug(github.context.payload)
-core.debug(github.context.event_name)
-core.debug(github.context.event)
+core.debug(github.context.payload.event_name)
+core.debug(github.context.payload.pull_request)
 // const { GITHUB_TOKEN } = process.env
 // const VERSIONS = ['patch', ]
 // const EVENT = 'pull_request'
