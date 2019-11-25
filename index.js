a7168c9ac604305c593c7d3c2220b9039a8dc7e2
paulo giusti <paulo.giusti@lemon.energy>
2019-11-25 18:24:36 -0300

    chore: echo pull request object

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const github = require('@actions/github')
 const { exec } = require('@actions/exec')
 
 const { GITHUB_TOKEN } = process.env
-const VERSIONS = ['patch', ]
+const VERSIONS = ['patch', 'minor', 'major']
 const EVENT = 'pull_request'
 const EVENT_ACTION = 'labeled'
 
 const checkEvent = () => {
   const { eventName, payload } = github.context
-
-  core.debug(eventName, payload.action)
 
   if (
     eventName === EVENT
@@ -22,9 +20,16 @@ const checkEvent = () => {
 }
 
 
+const getPullRequestVersion = async () => {
+  const { payload } = github.context
+
+  core.debug(JSON.stringfy(payload.pull_request))
+}
+
 const run = async () => {
   try {
     checkEvent()
+    const version = await getPullRequestVersion()
   } catch (e) {
     core.warning(e.message)
     return
