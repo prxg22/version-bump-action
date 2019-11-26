033ee7006897deb9cce5054986d34f106a48be6e
paulo giusti <paulo.giusti@lemon.energy>
2019-11-26 15:20:07 -0300

    chore: test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-// const { exec } = require("@actions/exec");
+const { exec } = require("@actions/exec");
 
 const EVENT = "pull_request";
 
