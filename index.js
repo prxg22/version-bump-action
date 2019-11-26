c48d0b33bf19ec6aeb517f2273f0f63ea18f6224
paulo giusti <paulo.giusti@lemon.energy>
2019-11-26 12:19:15 -0300

    chore: init version bump action

--- index.js
+++ index.js
@@ -2,54 +2,32 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const { exec } = require('@actions/exec')
 
-const { GITHUB_TOKEN } = process.env
-const VERSIONS = ['patch', 'minor', 'major']
 const EVENT = 'pull_request'
-const EVENT_ACTION = 'labeled'
 
 const checkEvent = () => {
   const { eventName, payload } = github.context
+  const prBaseBranch = payload.base.ref
+  const prHeadBranch = payload.head.ref
 
   if (
-    eventName === EVENT
-    && payload.action
-    && payload.action === EVENT_ACTION
+    eventName === EVENT &&
+    prBaseBranch === baseBranch &&
+    prHeadBranch === headBranch
   ) return
 
   throw Error('Event not supported')
 }
 
-
-const getPullRequestVersion = () => {
-  const { payload } = github.context
-
-  const { labels } = payload.pull_request
-
-  const version = VERSIONS.reduce((found, searched) => {
-    const label = labels.find(({ name }) => {
-      return searched === name.toLowerCase()
-    })
-    return (label && label.name) || found
-  }, undefined)
-
-  if (!version) throw Error('no semver label found!')
-
-  return version
-}
-
 const run = async () => {
+  const baseBranch = core.getInput('base-branch');
+  const headBranch = core.getInput('head-branch');
+  const githubToken = core.getInput('github-token');
+
   try {
     checkEvent()
-    const version = getPullRequestVersion()
   } catch (e) {
     core.warning(e.message)
     return
-  }
-
-  try {
-    await exec(`yarn version --${version} --no-git-tag-version`)
-  } catch (e) {
-    core.setFailed(e.message)  
   }
 }
 
